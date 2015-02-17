0c353b0546087fee9c6c3439ab481ca20cb9d545
deshraj <deshrajdry@gmail.com>
2015-02-18 00:49:58 +0530

    nodejs added

---
+++ installations/nodejs.sh
@@ -0,0 +1,6 @@
+# ubuntu installation
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+echo "##################### NODE.JS INSTALLED SUCCESSFULLY ################"
